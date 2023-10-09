29da7a8c161de6f992627a64ec017a0e2c7ff35d
thinh <thinh.phung@apacpro.com>
2023-10-09 17:11:06 +0700

    init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Navbar from './components/Navbar'
+
+export default function Home() {
+  return (
+    <div className='px-5 max-w-[1280px] mx-auto'>
+      <Navbar/>
+    </div>
+  )
+}
