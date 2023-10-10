9bf10bcfb1775a289b1583a3483f9be1be7eca03
thinh <thinh.phung@apacpro.com>
2023-10-10 17:00:29 +0700

    update register flow

--- app/signup/signUpForm.tsx
+++ app/signup/signUpForm.tsx
@@ -3,7 +3,6 @@ import React, {useState} from 'react';
 import {useRouter} from 'next/navigation';
 import Link from 'next/link';
 import axios from 'axios';
-import { log } from 'console';
 
 type Props = {}
 
